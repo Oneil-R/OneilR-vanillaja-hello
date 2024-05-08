95d2712bd322072190c4916e87d2f57c1768c760
Oneil-R <167453370+Oneil-R@users.noreply.github.com>
2024-05-08 23:42:39 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The Dog", "My grandma", "The mailman", "My bird", "Bobby"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was excercising",
+  "during my lunch",
+  "while I was praying"
+];
+function getRandomNum(a, b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
+function getRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = getRandomNum(min, max);
+  return anyArray[random];
+}
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
